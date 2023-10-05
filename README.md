6b2097daaf6993e2742a91ecf612781bfedd9554
Jacob Worsoe <jacobaw@stud.ntnu.no>
2023-10-05 19:06:46 +0200

    made code for multiindexing

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # stabekk
 
 For å ha noe her
+
+
+## Exploration of dates
+
+Two columns: *date_calc*, *date_forecast*. 
+
+15 minute intervals. 
+
+*date_calc* is the time at which the forecast is made always 7-8am day before *date_forecast*. Possible to look at time from forecast made as indicator of wheather reliability (maybe change the weighting of different models based on this weighting seasonality more or less as opposed to the forecasted weather features). For the time being we will not look at this.
--- ml_combat/data.py
+++ ml_combat/data.py
@@ -1,8 +1,8 @@
 """
 Contains the following functions:
 
-    * get_spreadsheet_cols - returns the column headers of the file
-    * main - the main function of the script
+    * get_training - gets full training data (duh)
+    * get_test - gets the feature estimates used for the forecast
 """
 
 import pandas as pd
@@ -11,26 +11,32 @@ from . import module_dir
 
 data_dir = module_dir + '/../data/'
 
-def test():
-    print(module_dir)
+
+def get_training():
+    """ gets full training data (duh) """
+
+    train_a = pd.read_parquet(data_dir + 'A/train_targets.parquet')
+    train_b = pd.read_parquet(data_dir + 'B/train_targets.parquet')
+    train_c = pd.read_parquet(data_dir + 'C/train_targets.parquet')
+
+    X_train_estimated_a = pd.read_parquet(data_dir + 'A/X_train_estimated.parquet')
+    X_train_estimated_b = pd.read_parquet(data_dir + 'B/X_train_estimated.parquet')
+    X_train_estimated_c = pd.read_parquet(data_dir + 'C/X_train_estimated.parquet')
+
+    X_train_observed_a = pd.read_parquet(data_dir + 'A/X_train_observed.parquet')
+    X_train_observed_b = pd.read_parquet(data_dir + 'B/X_train_observed.parquet')
+    X_train_observed_c = pd.read_parquet(data_dir + 'C/X_train_observed.parquet')
 
 
+def get_testing():
+    """ gets the feature estimates used for the forecast """
 
-train_a = pd.read_parquet(data_dir + 'A/train_targets.parquet')
-train_b = pd.read_parquet(data_dir + 'B/train_targets.parquet')
-train_c = pd.read_parquet(data_dir + 'C/train_targets.parquet')
+    X_test_estimated_a = pd.read_parquet(data_dir + 'A/X_test_estimated.parquet')
+    X_test_estimated_b = pd.read_parquet(data_dir + 'B/X_test_estimated.parquet')
+    X_test_estimated_c = pd.read_parquet(data_dir + 'C/X_test_estimated.parquet')
 
+    X_test_estimated_a.insert(2, 'location', 'A')
+    X_test_estimated_b.insert(2, 'location', 'B')
+    X_test_estimated_c.insert(2, 'location', 'C')
 
-X_train_estimated_a = pd.read_parquet(data_dir + 'A/X_train_estimated.parquet')
-X_train_estimated_b = pd.read_parquet(data_dir + 'B/X_train_estimated.parquet')
-X_train_estimated_c = pd.read_parquet(data_dir + 'C/X_train_estimated.parquet')
-
-
-X_train_observed_a = pd.read_parquet(data_dir + 'A/X_train_observed.parquet')
-X_train_observed_b = pd.read_parquet(data_dir + 'B/X_train_observed.parquet')
-X_train_observed_c = pd.read_parquet(data_dir + 'C/X_train_observed.parquet')
-
-
-X_test_estimated_a = pd.read_parquet(data_dir + 'A/X_test_estimated.parquet')
-X_test_estimated_b = pd.read_parquet(data_dir + 'B/X_test_estimated.parquet')
-X_test_estimated_c = pd.read_parquet(data_dir + 'C/X_test_estimated.parquet')
+    return pd.concat([X_test_estimated_a, X_test_estimated_b, X_test_estimated_c])
