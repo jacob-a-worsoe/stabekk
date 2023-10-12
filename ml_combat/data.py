e21b848a543e4f87281c6a06d705df497d762eca
Jacob Worsoe <jacobaw@stud.ntnu.no>
2023-10-12 15:01:05 +0200

    finish get_testing

--- ml_combat/data.py
+++ ml_combat/data.py
@@ -105,8 +105,27 @@ def get_testing():
     X_test_estimated_b = pd.read_parquet(data_dir + 'B/X_test_estimated.parquet')
     X_test_estimated_c = pd.read_parquet(data_dir + 'C/X_test_estimated.parquet')
 
-    X_test_estimated_a.insert(2, 'location', 'A')
-    X_test_estimated_b.insert(2, 'location', 'B')
-    X_test_estimated_c.insert(2, 'location', 'C')
+    ret = pd.DataFrame()
+    ret.set_index(pd.MultiIndex(levels=[[],[]], codes=[[],[]], names=['location', 'datetime']), inplace=True)
+    ret.columns = pd.MultiIndex(levels=[[],[], []], codes=[[],[], []], names=['feature_type', 'minutes', 'feature_name'])
 
-    return pd.concat([X_test_estimated_a, X_test_estimated_b, X_test_estimated_c])
+
+    # estimated data
+    data_dict = {'A': X_test_estimated_a, 'B': X_test_estimated_b, 'C': X_test_estimated_c}
+
+    for loc in data_dict:
+        out_temp = pd.DataFrame(data_dict[loc].date_forecast.dt.floor('H').unique(), columns=['date_forecast'])
+        out_temp.set_index('date_forecast', inplace=True)
+        out_temp.columns = pd.MultiIndex.from_product([[], [], out_temp.columns], names=['feature_type', 'minutes', 'feature_name'])
+
+        for m in [0, 15, 30, 45]:
+            temp = data_dict[loc][data_dict[loc].date_forecast.dt.minute == m].copy()
+            temp.set_index(temp.date_forecast.dt.floor('H'), inplace=True)
+            temp.drop(columns=['date_calc', 'date_forecast'], inplace=True)
+            temp.columns = pd.MultiIndex.from_product([['estimated'], [m], temp.columns], names=['feature_type', 'minutes', 'feature_name'])
+            out_temp = out_temp.merge(temp,left_index=True, right_index=True, how='outer')
+
+        out_temp.set_index(pd.MultiIndex.from_product([[loc], out_temp.index], names=['location', 'datetime']), inplace=True)
+        ret = pd.concat([ret, out_temp])
+
+    return ret
