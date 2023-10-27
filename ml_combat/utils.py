20811225ca9adbac79faa46979fbf6693d7911e4
Henrik Sontvedt <henriksontvedt@gmail.com>
2023-10-27 11:38:56 +0200

    Fixed interpolate_na

--- ml_combat/utils.py
+++ ml_combat/utils.py
@@ -4,12 +4,25 @@ import pandas as pd
 from . import module_dir
 
 
-def interpolate_na(self, df: pd.DataFrame, cols: list):
-    for col in cols:
-        df[col].ffill(inplace=True)
-        df[col].bfill(inplace=True)
+def interpolate_na(df: pd.DataFrame, cols: list, inplace=True):
+    if inplace:
+        for col in cols:
+            df[col].ffill(inplace=True)
+            df[col].bfill(inplace=True)
 
-        df[col].fillna(df[col].interpolate().cummax(), inplace=True)    
+            df[col].interpolate(inplace=True)   
+
+            df[col].fillna(df[col].cummax(), inplace=True)
+
+    else:
+        new_df = None
+        for col in cols:
+            new_df = df[col].ffill(inplace=inplace)
+            new_df = df[col].bfill(inplace=inplace)
+
+            new_df = df[col].fillna(df[col].interpolate().cummax(), inplace=inplace)
+
+        return new_df
 
 
 def map_hour_to_seasonal(df, time_col):
