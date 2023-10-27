cbaa7e2ec5880b423f3e77ad6d4a303dd9e1ec35
Jacob Worsoe <jacobaw@stud.ntnu.no>
2023-10-27 12:53:58 +0200

    add util for henrik

--- ml_combat/utils.py
+++ ml_combat/utils.py
@@ -31,3 +31,33 @@ def map_hour_to_seasonal(df, time_col):
                13: 11, 14: 10, 15: 9, 16: 8, 17: 7, 18: 6,
                19: 5, 20: 4, 21: 3, 22: 2, 23: 1, 24: 0,}
     df[time_col] = df[time_col].replace(mapping, inplace=True)
+
+
+# liste med 'y_pred' 
+
+def y_pred_to_csv(df, file_name):
+    df[['y_pred']].reset_index(drop=True).reset_index().rename(columns={'index': 'id', 'y_pred': 'prediction'}).to_csv(name, index=False)
+
+# def temp():
+#     df = ml.data.get_training_flattened()
+#     test = ml.data.get_testing_flattened()
+#     ret = pd.DataFrame()
+#     for location in ['A', 'B', 'C']:
+#         temp_df = df[df['location']==location]
+
+#         if location != "A":
+#             temp_df = temp_df[df.weather_data_type == 'observed']
+
+#         lr = ProphetModel()
+#         print("----------------- RESULT HERE", np.mean(lr.test(temp_df)))
+
+#         lr.train(temp_df)
+#         ret = pd.concat([ret, lr.predict(test[test['location']==location])])
+
+#     print("Done creating a linear regression model!")
+
+#     ret = ret.reset_index(drop=True).reset_index().drop(columns=["ds"]).rename(columns={'index': 'id', 'y_pred': 'prediction'})
+#     ret.prediction = ret.prediction.apply(lambda a : max(a, 0))
+#     ret
+
+#     ret.to_csv("prophet_tweaked.csv", index=False)
